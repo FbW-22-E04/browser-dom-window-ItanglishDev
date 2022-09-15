f2d14c7d7048dbf94c82a0e184f434c03fd07762
chris <chrishethertonweb@gmail.com>
2022-09-15 14:01:19 +0200

    created function that holds prompts and conditions

---
+++ main.js
@@ -0,0 +1,25 @@
+
+const number = Math.floor(Math.random() * 9 + 1)
+console.log(number);
+let counter = 0
+
+function checkForAnswer() {
+
+  if (counter !== 3) {
+    let answer = prompt('Guess a number between 1 - 10')
+    // console.log(number);
+    counter++
+
+    if (answer === number) {
+
+      prompt('Success, the number was', number, '!', 'Attempts', counter);
+
+    } else {
+
+      prompt('Wrong!!! Try Again!')
+      checkForAnswer()
+    }
+  } prompt('Sorry, you failed to guess the number in three attempts. The number was ' + number)
+}
+
+checkForAnswer()
