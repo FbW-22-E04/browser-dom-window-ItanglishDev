e8562632a982de0eed50b4a29fe464e5f4c13653
chris <chrishethertonweb@gmail.com>
2022-09-15 14:06:49 +0200

    obtained failed prompt after 3 attempts

--- main.js
+++ main.js
@@ -5,21 +5,23 @@ let counter = 0
 
 function checkForAnswer() {
 
-  if (counter !== 3) {
+  if (counter < 3) {
     let answer = prompt('Guess a number between 1 - 10')
     // console.log(number);
     counter++
 
-    if (answer === number) {
-
-      prompt('Success, the number was', number, '!', 'Attempts', counter);
-
-    } else {
+    if (number !== answer) {
 
       prompt('Wrong!!! Try Again!')
       checkForAnswer()
+
+    } else {
+
+      prompt('Success, the number was', number, '!', 'Attempts', counter);
+
     }
   } prompt('Sorry, you failed to guess the number in three attempts. The number was ' + number)
+  return
 }
 
 
